c4a53d0e33a599c90c74880e3fa4cbde5368025a
Yong <amico741@naver.com>
2022-07-05 01:48:24 +0900

    JWT Token 발행

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { UsersModule } from 'src/users/users.module';
 import { AuthService } from './auth.service';
+import { jwtConstants } from './constant';
 import { LocalStrategy } from './local.strategy';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [
+    UsersModule,
+    AuthModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
   providers: [AuthService, LocalStrategy],
+  exports: [AuthService],
 })
 export class AuthModule {}
---
+++ src/auth/constant.ts
@@ -0,0 +1,4 @@
+export const jwtConstants = {
+  // secret키는 절대로 외부에 노출되서는 안된다.
+  secret: 'wantedTeam4',
+};
